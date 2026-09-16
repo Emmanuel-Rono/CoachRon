be058bddcb70a215fb0859be9b5e85ec0312dde1
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-09-16 11:30:47 +0300

    chore: OB backend

--- backend/app/services/coaching_service.py
+++ backend/app/services/coaching_service.py
@@ -5,23 +5,20 @@ from app.prompts.coach_prompt import Coach_Ron_System_Prompt
 from app.schemas.coach import CoachResponse
 from app.services.ollama_client import get_ollama_client
 
-
-
 class CoachingService:
-    def __init__(self) -> None:
-        model = get_ollama_client()
-        self.coach_model = model.with_structured_output(
-            CoachResponse,
-            method="json_schema",
-        )
-
-    def coach(self, message: str) -> CoachResponse:
-        return self.coach_model.invoke(
+    def __init__(self):
+        model= get_ollama_client()
+        self.OllamaModel = model.with_structured_output(
+            CoachResponse, method ="json_schema")
+        
+    def get_coach_response(self, message: str) -> CoachResponse:
+        return self.OllamaModel.invoke(
             [
                 SystemMessage(content=Coach_Ron_System_Prompt),
-                HumanMessage(content=message),
+                HumanMessage(content= message),
             ]
         )
+
 
 
 coaching_service = CoachingService()
